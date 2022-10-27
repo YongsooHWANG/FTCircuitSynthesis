be8c764fe9a5ebaaaa6671903bcf98022ae8cb7e
Yongsoo Hwang <116055087+YongsooHWANG@users.noreply.github.com>
2022-10-27 11:41:25 +0900

    Update DirectedAcyclicGraph.py

--- ftsynthesis/DirectedAcyclicGraph.py
+++ ftsynthesis/DirectedAcyclicGraph.py
@@ -1,5 +1,12 @@
 # -*-coding:utf-8-*-
 
+# This code is part of ftsynthesis (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
+#
+# Copyright 2022 ETRI
+#
+# This code is licensed under the BSD-3-Clause.
+
+
 import collections
 import itertools
 import parse
