bb832f95e31bd951c9a662f6b8d890abab6ca65c
Yongsoo Hwang <116055087+YongsooHWANG@users.noreply.github.com>
2022-10-27 11:42:21 +0900

    Update __init__.py

--- ftsynthesis/__init__.py
+++ ftsynthesis/__init__.py
@@ -1,3 +1,12 @@
 
+# -*-coding:utf-8-*-
+
+# This code is part of ftsynthesis (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
+#
+# Copyright 2022 ETRI
+#
+# This code is licensed under the BSD-3-Clause.
+
+
 __all__ = ["checkup", "depth_analysis", "DirectedAcyclicGraph", "DistanceMatrix", "error", "formatconversion",
 			"globalVariable", "ftsynthesis", "SABRE_utility", "util"]
