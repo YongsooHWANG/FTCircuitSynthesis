4af63bbeba7e82beea5ada290e0201d7fc1f7692
Yongsoo Hwang <116055087+YongsooHWANG@users.noreply.github.com>
2022-10-27 11:42:05 +0900

    Update SABRE_utility.py

--- ftsynthesis/SABRE_utility.py
+++ ftsynthesis/SABRE_utility.py
@@ -1,4 +1,13 @@
 
+# -*-coding:utf-8-*-
+
+# This code is part of ftsynthesis (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
+#
+# Copyright 2022 ETRI
+#
+# This code is licensed under the BSD-3-Clause.
+
+
 import re
 import collections
 import parse
