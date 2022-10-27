cbcedf7d9df394e4509267605189bbae59e404df
Yongsoo Hwang <116055087+YongsooHWANG@users.noreply.github.com>
2022-10-27 11:46:33 +0900

    Update util.py

--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,13 @@
 
 # -*-coding:utf-8-*-
 
+# This code is part of ftsynthesis (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
+#
+# Copyright 2022 ETRI
+#
+# This code is licensed under the BSD-3-Clause.
+
+
 import os
 import sys
 import simplejson as json
